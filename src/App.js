3b121c8bb1560949076e3e9d7a684d77f1cffd95
Rachel Brown Gomez (Ray) <rgbrown21@icloud.com>
2023-11-08 22:52:44 -0500

    section 1 of 3 for hw5 complete

--- src/App.js
+++ src/App.js
@@ -5,10 +5,27 @@ import AddTodoForm from "./AddTodoForm.js";
 
 
 const App = () => {
-  const [todoList, setTodoList] = React.useState([]);
+  const [todoList, setTodoList] = React.useState(() => {
+    const savedTodoList = JSON.parse(localStorage.getItem('savedTodoList'));
+    return savedTodoList || [];
+  });;
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo]);
   }
+
+
+  React.useEffect(() => {
+    const stringifyTodoList = JSON.stringify(todoList);
+    localStorage.setItem('savedTodoList', stringifyTodoList);
+  }, [todoList]);
+
+  // React.useEffect(() => {
+  //   const savedTodoList = JSON.parse(localStorage.getItem('savedTodoList'));
+  //   if (savedTodoList) {
+  //     setTodoList(savedTodoList);
+  //   }
+  // }, []);
+
   return (
     <div style={{ textAlign: 'left' }}>
       <h1>Anime to watch List</h1>
