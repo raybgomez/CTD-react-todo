158a9672bf1632b93765b91f0397adc09f3b9825
Rachel Brown Gomez (Ray) <rgbrown21@icloud.com>
2024-08-28 23:51:32 -0400

    Updates

---
+++ src/Components/TodoList.js
@@ -0,0 +1,23 @@
+import TodoListItem from "./TodoListItem.js";
+import propTypes from 'prop-types';
+
+
+const TodoList = ({ todoList, onRemoveTodo }) => {
+    return (<ul>
+        {
+            todoList.map(todo => (
+                <TodoListItem
+                    key={todo.id}
+                    todo={todo}
+                    onRemoveTodo={onRemoveTodo} />)
+            )}
+    </ul>)
+
+}
+
+TodoList.propTypes = {
+    todoList: propTypes.array.isRequired,
+    onRemoveTodo: propTypes.func.isRequired,
+}
+
+export default TodoList
